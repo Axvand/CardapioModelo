c2c3d3e4f88bb840e2ae9d30b376d2bad908717e
Alex Rocha <131891153+Axvand@users.noreply.github.com>
2023-08-21 20:59:22 -0300

    commit ajuste
    ajuste no main.js

--- main.js
+++ main.js
@@ -16,3 +16,65 @@ menu.addEventListener('click',()=>{
             menu.style.display='none'
         },500)
 })
+
+
+const imgCard = document.querySelectorAll('.imgCard');
+const pratoCard = document.querySelectorAll('.PratoCard');
+const ingredientesCard = document.querySelectorAll('.ingredientesCard');
+const precoCard = document.querySelectorAll('.precoCard');
+const Card = document.querySelectorAll('.card')
+
+const pedido =[];
+
+const buttonPush = document.querySelectorAll('.PushCard');
+
+function setAttribute(Atributo, valor, Elemento){
+    const atributo = document.createAttribute(Atributo);
+    atributo.value = `${valor}`;
+    Elemento.setAttributeNode(atributo);
+};
+
+    console.log(Card)
+
+fetch('./pizzas.json', {
+    headers: {
+        accept: "application/json"
+    }
+}
+).then(res => res.json()).then((res) => {
+    const pizzas = res.pizzas
+
+    for (x = 0; x < 9; x++) {
+        setAttribute('src', pizzas[x].img, imgCard[x]);
+        pratoCard[x].innerHTML = `${pizzas[x].prato}`;
+        ingredientesCard[x].innerHTML = `${pizzas[x].description}`;
+        precoCard[x].innerHTML = `${pizzas[x].preco}`;
+
+        buttonPush[x].addEventListener('click', () => {
+            pedido.push(pizzas[0])
+            console.log(pedido)
+        })
+    }
+    
+});
+
+
+/*
+imgCard
+PratoCard
+ingredientesCard
+precoCard
+
+ for (x = 0; x < 9; x++) {
+        setAttribute('src', pizzas[x].img, imgCard[x]);
+        pratoCard[x].innerHTML = `${pizzas[x].prato}`;
+        ingredientesCard[x].innerHTML = `${pizzas[x].description}`;
+        precoCard[x].innerHTML = `${pizzas[x].preco}`;
+
+        buttonPush[x].addEventListener('click', () => {
+            pedido.push(pizzas.id)
+            console.log(pedido)
+        })
+    }
+*/
+//const x = document.querySelectorAll('ccc')
