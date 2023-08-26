7e426bc8108b052df55cfe0f483f83e5e81537a4
Alex Rocha <131891153+Axvand@users.noreply.github.com>
2023-08-26 09:14:49 -0300

    commit de construção
    ajustes nas funcionalidades do carrinho

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ const menu = document.querySelector('.menu')
 const menuMain = document.querySelector('.mainMenu')
 
 //ShowMenu
-boxMenu2.addEventListener('click',()=>{
-    menu.style.display='block'
-    menuMain.style.display='block'
-    menuMain.style.animation='fadeInRight'
-    menuMain.style.animationDuration='0.7s'
-    boxMenu2.style.backgroundColor='yellow'
+boxMenu2.addEventListener('click', () => {
+    menu.style.display = 'block'
+    menuMain.style.display = 'block'
+    menuMain.style.animation = 'fadeInRight'
+    menuMain.style.animationDuration = '0.7s'
+    boxMenu2.style.backgroundColor = 'yellow'
 })
 //CloseMenu
-menu.addEventListener('click',()=>{
-    menuMain.style.animation='fadeOutRight'
-    menuMain.style.animationDuration='0.6s'
-    setTimeout(()=>{
-            boxMenu2.style.backgroundColor='transparent'
-            menu.style.display='none'
-        },500)
+menu.addEventListener('click', () => {
+    menuMain.style.animation = 'fadeOutRight'
+    menuMain.style.animationDuration = '0.6s'
+    setTimeout(() => {
+        boxMenu2.style.backgroundColor = 'transparent'
+        menu.style.display = 'none'
+    }, 500)
 })
 const imgCard = document.querySelectorAll('.imgCard');
 const pratoCard = document.querySelectorAll('.PratoCard');
@@ -37,23 +37,23 @@ const carrinhoButton = document.querySelector('.boxNav3')
 //mainCarrinho
 const mainCarrinho = document.querySelector('.mainCarrinho')
 //Indicadpr de total dentro do menu carrinho
-const totalValorCart =document.querySelector('.totalValorCart')
+const totalValorCart = document.querySelector('.totalValorCart')
 //====================================
 
-const pedido =[];
-const totalObj =[]
+const pedido = [];
+const totalObj = []
 const buttonPush = document.querySelectorAll('.PushCard');
 //caixa de pedidos
 const boxPedidos = document.querySelector('.boxPedidosMenuCart')
 
 //setarAtrinutoEmElementos
-function setAttribute(Atributo, valor, Elemento){
+function setAttribute(Atributo, valor, Elemento) {
     const atributo = document.createAttribute(Atributo);
     atributo.value = `${valor}`;
     Elemento.setAttributeNode(atributo);
 };
 //setInCar
-function setInCar(button, array,total, pizza) {
+function setInCar(button, array, total, pizza) {
     button.addEventListener('click', () => {
         //inclusao na array produtos
         //indicador de qtd carrinho
@@ -68,61 +68,61 @@ function setInCar(button, array,total, pizza) {
         boxAlert.style.display = 'block'
 
         //condicional do indicador de quantidade
-        if(pedido.length>0){
-            identificadorQtd.style.display='block'
+        if (pedido.length > 0) {
+            identificadorQtd.style.display = 'block'
         }
         //indicado de quatidade
-        document.querySelector(".identificadorQtdCarrinho").innerHTML=`${pedido.length}`
-       //saida do alerta animação
+        document.querySelector(".identificadorQtdCarrinho").innerHTML = `${pedido.length}`
+        //saida do alerta animação
         setTimeout(() => {
             boxAlert.style.animation = 'fadeOut'
             boxAlert.style.animationDuration = '0.6s'
             setTimeout(() => {
                 boxAlert.style.display = 'none'
-            },600)
+            }, 600)
         }, 1000)
 
         //incrementar o total
         sum = 0;
         for (var x = 0; x < pedido.length; x++) {
             sum += pedido[x].preco
-            ;
+                ;
         }
 
-        document.querySelector('.totalValorCart').innerHTML=`R$${sum},00`;
+        document.querySelector('.totalValorCart').innerHTML = `R$${sum},00`;
 
         console.log(sum)
 
 
-      // renderProdutono carrinho 
-         
+        // renderProdutono carrinho 
+
         addBox()
-        for(var i =0; i<pedido.length; i++){
-            txtAdd(i,pedido[i].prato)
-            addImg(pedido[i].img ,i)
+        for (var i = 0; i < pedido.length; i++) {
+            txtAdd(i, pedido[i].prato)
+            addImg(pedido[i].img, i)
         }
     })
 };
 
 //showCart
-carrinhoButton.addEventListener('click',()=>{
-    carrinhoMenu.style.display='block'
-    mainCarrinho.style.animation='fadeInLeft'
-    mainCarrinho.style.animationDuration='1s'
-    mainCarrinho.style.display='block'
-    carrinhoButton.style.backgroundColor='yellow'
-     
+carrinhoButton.addEventListener('click', () => {
+    carrinhoMenu.style.display = 'block'
+    mainCarrinho.style.animation = 'fadeInLeft'
+    mainCarrinho.style.animationDuration = '1s'
+    mainCarrinho.style.display = 'block'
+    carrinhoButton.style.backgroundColor = 'yellow'
+
 })
 //closeCart
-mainCarrinho2.addEventListener('click',()=>{
-    mainCarrinho.style.animation='fadeOutLeft'
-    mainCarrinho.style.animationDuration='0.5s'
-    mainCarrinho.style.display='block'
-    carrinhoButton.style.backgroundColor='transparent'
-    setTimeout(()=>{
-        carrinhoMenu.style.display='none'
+mainCarrinho2.addEventListener('click', () => {
+    mainCarrinho.style.animation = 'fadeOutLeft'
+    mainCarrinho.style.animationDuration = '0.5s'
+    mainCarrinho.style.display = 'block'
+    carrinhoButton.style.backgroundColor = 'transparent'
+    setTimeout(() => {
+        carrinhoMenu.style.display = 'none'
 
-    },500)
+    }, 500)
 })
 //================================
 
@@ -133,13 +133,13 @@ fetch('./pizzas.json', {
         accept: "application/json"
     }
 })
-.then(res => res.json()).then((res) => {
-    const pizzas = res.pizzas
-    for(var x = 0 ; x < pizzas.length ; x++){
-    setInCar(buttonCard[x], pedido, totalObj, pizzas[x])
-    }
+    .then(res => res.json()).then((res) => {
+        const pizzas = res.pizzas
+        for (var x = 0; x < 10; x++) {
+            setInCar(buttonCard[x], pedido, totalObj, pizzas[x])
+        }
 
-});
+    });
 
 
 
@@ -159,26 +159,26 @@ function addBox() {
     setAttribute('class', 'textPedido', txt);
 
     //Delete produto Carrinho:
-    const imgDeleteUrl = 'https://cdn-icons-png.flaticon.com/512/3221/3221803.png'
-    const imgDelete = document.createElement('img');
-    box.appendChild(imgDelete);
-    setAttribute('class', 'imgDelete', imgDelete);
-    setAttribute('height', '25px', imgDelete);
-    setAttribute('width', '25px', imgDelete);
-    setAttribute('src', imgDeleteUrl, imgDelete);
-
-    imgDelete.addEventListener('click', () => {
-        boxPedidos.removeChild(box)
-        pedido.pop()
-        //contador do carrinho..
-        document.querySelector(".identificadorQtdCarrinho").innerHTML = `${pedido.length}`
-
-        console.log(box)
-        console.log(pedido)
-
-       // document.querySelector('.totalValorCart').innerHTML=`R$${sum-preço},00`;
-
-    })
+    /* const imgDeleteUrl = 'https://cdn-icons-png.flaticon.com/512/3221/3221803.png'
+     const imgDelete = document.createElement('img');
+     box.appendChild(imgDelete);
+     setAttribute('class', 'imgDelete', imgDelete);
+     setAttribute('height', '25px', imgDelete);
+     setAttribute('width', '25px', imgDelete);
+     setAttribute('src', imgDeleteUrl, imgDelete);
+ 
+     imgDelete.addEventListener('click', () => {
+         boxPedidos.removeChild(box[box.length - 1])
+         pedido.pop()
+         //contador do carrinho..
+         document.querySelector(".identificadorQtdCarrinho").innerHTML = `${pedido.length}`
+ 
+         console.log(box)
+         console.log(pedido)
+ 
+         document.querySelector('.totalValorCart').innerHTML=`R$ ${sum},00`;
+ 
+     })*/
 }
 // função set>Nome dos pratos no carrinho
 function txtAdd(y, NomeDoPrato) {
@@ -192,3 +192,28 @@ function addImg(imgUrl, i) {
     atributo.value = `${imgUrl}`;
     imgCart[i].setAttributeNode(atributo);
 }
+
+const imgDelete = document.querySelector('.imgDelete')
+
+
+imgDelete.addEventListener('click', () => {
+    const box = document.querySelectorAll('.produtos')
+    pedido.pop()
+    console.log(box)
+    boxPedidos.removeChild(box[box.length - 1])
+
+
+    som = 0;
+    for (var x = 0; x < pedido.length; x++) {
+        som += pedido[x].preco
+            ;
+    }
+
+    document.querySelector('.totalValorCart').innerHTML = `R$ ${som},00`
+    if (pedido.length >= 0) {
+        document.querySelector(".identificadorQtdCarrinho").style.display = 'none';
+    } else if (pedido.length > 0) {
+        document.querySelector(".identificadorQtdCarrinho").innerHTML = `${pedido.length}`
+    }
+
+})
